437ed9237f117dcb73465d8d7612da5cfe7c441d
void-maxxer <apex.ender@example.com>
2026-08-16 20:59:53 +0200

    C4L2: block type pointer built, tested

--- src/blocknode.py
+++ src/blocknode.py
@@ -1,3 +1,4 @@
+from enum import Enum
 
 def markdown_to_blocks(markdown):
 	marked = []
@@ -6,3 +7,45 @@ def markdown_to_blocks(markdown):
 		if blok != "":
 			marked.append(blok)
 	return marked
+
+class BlockType(Enum):
+	BLOCK = "paragraph"
+	HEAD = "heading"
+	CODE = "code"
+	QUOTE = "quote"
+	UNORD_LST = "unordered_list"
+	ORDER_LST = "ordered_list"
+
+def block_to_block_type(md_block: str) -> "BlockType":
+	hash = ("# ", "## ", "### ", "#### ", "##### ", "###### ")
+	validQ = True
+	validU = True
+	validO = True
+	num = 1
+	if md_block.startswith(hash):
+		return BlockType.HEAD
+	if md_block.startswith("```\n") and md_block.endswith("\n```"):
+		return BlockType.CODE
+	if md_block.startswith(">"):
+		for lin in md_block.split("\n"):
+			if not lin.startswith(">"):
+				validQ = False
+				break
+		if validQ:
+			return BlockType.QUOTE
+	if md_block.startswith("- "):
+		for lin in md_block.split("\n"):
+			if not lin.startswith("- "):
+				validU = False
+				break
+		if validU:
+			return BlockType.UNORD_LST
+	if md_block.startswith(f"{num}. "):
+		for lin in md_block.split("\n"):
+			if not lin.startswith(f"{num}. "):
+				validO = False
+				break
+			num += 1
+		if validO:
+			return BlockType.ORDER_LST
+	return BlockType.BLOCK
--- src/test_blocknode.py
+++ src/test_blocknode.py
@@ -1,5 +1,5 @@
 import unittest
-from blocknode import markdown_to_blocks
+from blocknode import markdown_to_blocks, BlockType, block_to_block_type
 
 class TestMarkdownToBlocks(unittest.TestCase):
     def test_markdown_to_blocks(self):
@@ -75,3 +75,62 @@ This is the same paragraph on a new line
                 "- This is a list\n- with items",
             ],
         )
+
+class TestBlockToBlockType(unittest.TestCase):
+    def test_btbt_base(self):
+        self.assertEqual(block_to_block_type("This is a paragraph."), BlockType.BLOCK)
+
+    def test_btbt_face(self):
+        self.assertEqual(block_to_block_type("#### That, a heading."), BlockType.HEAD)
+
+    def test_btbt_daze(self):
+        block = """```
+trigger = False
+tik = float("-inf")
+while True:
+    if trigger:
+        break
+    if tik > 0:
+        trigger = True
+    tik += 10^10
+```"""
+        self.assertEqual(block_to_block_type(block), BlockType.CODE)
+
+    def test_btbt_sayz(self):
+        block = """> This is a wise saying.
+> Continued on the next line.
+> And wrapped up here."""
+        self.assertEqual(block_to_block_type(block), BlockType.QUOTE)
+
+    def test_btbt_pace(self):
+        block = """- This is a wise saying.
+- Continued on the next line.
+- And wrapped up here."""
+        self.assertEqual(block_to_block_type(block), BlockType.UNORD_LST)
+
+    def test_btbt_case(self):
+        block = """1. First step
+2. Second step
+3. Third step"""
+        self.assertEqual(block_to_block_type(block), BlockType.ORDER_LST)
+
+    def test_btbt_mace(self):
+        block = """> This is a wise saying.
+ Continued on the next line.
+> And wrapped up here."""
+        self.assertEqual(block_to_block_type(block), BlockType.BLOCK)
+
+    def test_btbt_vase(self):
+        block = """- This is a wise saying.
+- Continued on the next line.
+ And wrapped up here."""
+        self.assertEqual(block_to_block_type(block), BlockType.BLOCK)
+
+    def test_btbt_lace(self):
+        block = """1. First step
+2. Second step
+4. Third step"""
+        self.assertEqual(block_to_block_type(block), BlockType.BLOCK)
+
+    def test_btbt_fels(self):
+        self.assertEqual(block_to_block_type(""), BlockType.BLOCK)
